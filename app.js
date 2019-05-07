ba6693299f701248f19dffaa6e0f633b43a9243b
HanadAhmed <49880247+HanadAhmed@users.noreply.github.com>
2019-05-07 13:50:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 AWS.config.update({
-    region: "us-east-1",
-    endpoint: "dynamodb.us-east-1.amazonaws.com",
-    accessKeyId  : "ASIARV4AYRH3HPQQL4VI",
-    secretAccessKey : "JkIsnRDGNHUqPhVOmn77g/Cb5Hc74X3oirmIBBLe",
-    sessionToken : "FQoGZXIvYXdzEDkaDBUK0iD8scHZpjhbhSKFA05AT1nhZxSSudPs9iMEACgZH+hbn2dVzUi2UMHETKebkZaeM7XtIt7WFiaZFFc74W7TDdM3LmOAFGYagcD8fsZSEzYvXIFZoy8U8ZUeuLKEDShyjRcK/yDTaoCBNwKpHEg8zLssE4RHT7NYkdCSZCy8deCUeTCO4+ZpwY5S6fdbzW4pFtqVW7+TM9rtj/IMjYkSJBRD38q4QEUuCLvq44QPz7MrLgogenSVTLULs56K9YodMuRuFezodtodT5JZhNKD/ZIWhgYt7d3nS6UK39qE7sEPEZkofnVHHyCGbiCTyBltu2J1lIm7pnIT8rSfz7RFvOrzaICYZaWzZItzScoWAQzpz6OuIa01S2R6jhOQf9zxl+viNU2mAXgLoSolA1MxBfQblYzCrwFVB7uXjZor/3Z6/JocpoSrk48UdNWW6H5oqrFjoJUQfLwOwGbjcV8zt8EIamhPx+bWEEwqNzbsSZqYPN1wdEJPAlAeMrC1DtS5DOvkp7L6bgYRQrBQ63KYQ4JmKJ2Mw+YF"
+    "region": "us-east-1",
+    "endpoint" : "dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId"  : "ASIARV4AYRH3HPQQL4VI",
+    "secretAccessKey" : "JkIsnRDGNHUqPhVOmn77g/Cb5Hc74X3oirmIBBLe"
 });
 
 console.log(process.env.AWS_SECRET_ACCESS_KEY);
